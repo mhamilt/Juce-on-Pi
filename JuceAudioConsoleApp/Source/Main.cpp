28d6afb252354c705d4acd2a6a7a9a9f1970db5f
mhamilt <mhamilton942@gmail.com>
2018-10-29 17:54:35 +0000

    wiringpi test

--- JuceAudioConsoleApp/Source/Main.cpp
+++ JuceAudioConsoleApp/Source/Main.cpp
@@ -10,17 +10,22 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "AudioProcessing.hpp"
+#include <wiringPi.h>
 //==============================================================================
 int main (int argc, char* argv[])
 {
     AudioProcessing processor;
+    wiringPiSetup () ;
+    pinMode (0, OUTPUT) ;
+    
     while (true)
     {
+        digitalWrite (0, HIGH) ; delay (500) ;
+        printf("on\n");
+        digitalWrite (0,  LOW) ; delay (500) ;
+        printf("off\n");
     }
     return 0;
 }
-
 //==============================================================================
-
-
 
